018a7e370225fd0b02dc4c29feb177a581da7897
JuliyaPershina <jpershina79@gmail.com>
2025-10-22 20:55:21 +0300

    Setting up the postgres+ drizzle.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // @ts-ignore // Next.js experimental settings
+    allowedDevOrigins: [
+      'http://localhost:3000',
+      'http://192.168.1.69:3000', // твій локальний IP
+    ],
+  },
 };
 
 export default nextConfig;
