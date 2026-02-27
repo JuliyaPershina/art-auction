f0050175f263055dc07c0160479356bc7740e553
JuliyaPershina <jpershina79@gmail.com>
2026-02-27 23:02:33 +0200

    feat(i18n): update layout and metadata to support async locale params

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,21 @@
 import { Header } from '@/components/Header';
 import type { Metadata } from 'next';
+import { ReactNode } from 'react';
+
+type Locale = 'hu' | 'en';
+
+type LayoutProps = {
+  children: ReactNode;
+  params: Promise<{ locale: Locale }>; // асинхронний params
+};
+
+// Гард для перевірки допустимих локалей
+const isLocale = (l: string): l is Locale => l === 'hu' || l === 'en';
 
 export async function generateMetadata({
   params,
 }: {
-  params: Promise<{ locale: 'hu' | 'en' }>;
+  params: Promise<{ locale: Locale }>;
 }) {
   const { locale } = await params;
 
@@ -26,14 +37,27 @@ export async function generateMetadata({
   };
 }
 
-export default async function LocaleLayout({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: Promise<{ locale: 'hu' | 'en' }>;
-  }) {
+// export default async function LocaleLayout({
+//   children,
+//   params,
+// }: {
+//   children: React.ReactNode;
+//   params: Promise<{ locale: 'hu' | 'en' }>;
+//   }) {
+//   const { locale } = await params;
+//   return (
+//     <>
+//       <Header locale={locale} />
+//       <main className="min-h-screen">{children}</main>
+//     </>
+//   );
+// }
+
+export default async function LocaleLayout({ children, params }: LayoutProps) {
   const { locale } = await params;
+
+  if (!isLocale(locale)) throw new Error(`Invalid locale: ${locale}`);
+
   return (
     <>
       <Header locale={locale} />
